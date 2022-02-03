702a7a51dd7aadf35aa1fcfcf1a5bd17c636c3eb
Oskar <woozzaang@gmail.com>
2022-02-03 23:42:31 +0900

    📖 커스텀 클래스 생성 및 씬과 연결

---
+++ RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MemoDetailViewController.swift
+//  RxMemo
+//
+//  Created by Park Woo Chan on 2022/02/03.
+//
+
+import UIKit
+
+class MemoDetailViewController: UIViewController {
+
+}
---
+++ RxMemo/RxMemo/ViewController/MemoListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MemoListViewController.swift
+//  RxMemo
+//
+//  Created by Park Woo Chan on 2022/02/03.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+}
