c5a8e2f9e35603391f47d4d5fc754defd9cfb932
Oskar <woozzaang@gmail.com>
2022-02-03 23:42:31 +0900

    📖 커스텀 클래스 생성 및 씬과 연결

---
+++ RxMemo/RxMemo/ViewController/MemoComposeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoComposeViewController.swift
+//  RxMemo
+//
+//  Created by Park Woo Chan on 2022/02/03.
+//
+
+import UIKit
+
+class MemoComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
